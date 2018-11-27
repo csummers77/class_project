72c42a99fa951201c0f951a02a3f377cec7b8b79
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Air.local>
2018-11-27 15:04:00 -0500

    notes

---
+++ notes.py
@@ -0,0 +1,27 @@
+# What we are using in pygame.
+
+#         Game events>>>
+#         shooter
+#         meteors
+#         explosions>>Blows up
+#         collision detection for user
+#         enemy
+#         player
+#         background
+#         start button
+#         restart
+
+# screen size = 620,480
+
+# Who's doing what??
+# Cody == image resizing
+# chris == figure out background, sound effects
+# jim == the coder
+
+# Game overview!!!!
+# 3 lives
+
+
+# other
+# player 1 stays with confined area
+        
